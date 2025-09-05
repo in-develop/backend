c646c83260efbb6bc2e229d7b81dca2230da998c
Яропуд Вова <bratiskavova@gmail.com>
2025-09-05 09:04:02 +0300

    Add ResendEmailRequest class for email resending
    Introduced a new class `ResendEmailRequest` in the
    `TeamChallenge.Models.Requests.Login` namespace. This class
    includes two required properties: `Email` and `ClientUrl`,
    both of type `string`, to facilitate the process of
    resending emails by capturing necessary user information.

---
+++ TeamChallegne.Models/Requests/Login/ResendEmailRequest.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TeamChallenge.Models.Requests.Login
+{
+    public class ResendEmailRequest
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string ClientUrl { get; set; }
+    }
+}
